08f827915ebc26b67e322841143837be757dfac4
szabadee <kreativ.szabadi@gmail.com>
2018-12-06 16:44:35 +0100

    token implemented

--- controllers/users.js
+++ controllers/users.js
@@ -2,19 +2,55 @@ const express = require('express');
 const users = express();
 const models = require('../models');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 // create
 users.post('/', (req, res) => {
-  bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-      req.body.encryptedPassword = hash;
-      models.User.create(req.body)
-        .then(user => {
-          res.json(user);
+  models.User.findOne({ where: { username: req.body.username } })
+    .then(user => {
+      if (user) {
+        return res.status(400).send('Invalid username');
+      }
+      bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+          req.body.encryptedPassword = hash;
+          models.User.create(req.body)
+            .then(user => {
+              res.json(user);
+            });
         });
-    });  
+    });
 });
 
+  users.post('/login', (req, res) => {
+    models.User.findOne({
+      where: { username: req.body.username }
+    }).then(user => {
+      if (!user) {
+        return res.status(400).send('Invalid User')
+      }
+      bcrypt.compare(req.body.password, user.encryptedPassword)
+        .then(isValid => {
+          if (!isValid) {
+            return res.status(401).send('Invalid password');
+          }
+          const token = jwt.sign({ id: user.id }, process.env.SECRET, { expiresIn: '1h' }).toString();
+          res.json({ token: token });
+        });
+    });
+  });
 
-module.exports = users;
+  users.get('/me', (req, res) => {
+    res.json(req.user);
+  });
+
+  users.put('/me', (req, res) => {
+    req.user.update(req.body)
+      .then(user => {
+        res.json(user);
+      });
+  });
+
+
+  module.exports = users;
--- index.js
+++ index.js
@@ -3,14 +3,36 @@ const app = express();
 const bodyParser = require('body-parser');
 const tasks = require('./controllers/tasks');
 const users = require('./controllers/users');
+const jwt = require('jsonwebtoken');
+const models = require('./models');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  if (req.header('x-auth')) {
+    let token = req.header('x-auth');
+    console.log(token);
+    let decoded;
+    try {
+      decoded = jwt.verify(token, process.env.SECRET);
+    } catch(err) {
+      return res.status(401).send(err);
+    }
+    models.User.findById(decoded.id)
+      .then(user => {
+        req.user = user;
+        next();
+      });
+  } else {
+    next();
+  }
+});
 
 app.use('/tasks', tasks);
 app.use('/users', users);
 
 app.listen(3002, () => {
-  console.log('Run on 3002');
+  console.log('Hello Peet, I am ready and run on 3002');
 });
 
 // app.listen(process.env.PORT);
